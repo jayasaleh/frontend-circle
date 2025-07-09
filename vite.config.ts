d916356d233fd6af72c16d657c0975d5f99d64fd
jayasaleh <salehjaya97@gmail.com>
2025-07-09 17:13:42 +0800

    define config: add base process env

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -11,4 +10,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  base: process.env.VITE_BASE_PATH || '/frontend-circle',
 });
